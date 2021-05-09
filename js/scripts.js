5a199afea6ee6877cb8a790a1e54b9aca7c7b936
Urjeet Deshmukh <60583154+urjeet@users.noreply.github.com>
2021-05-09 11:16:47 -0400

    Reinstated documentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+/*!
+    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+/*!
+ * @author modified by Urjeet Deshmukh - @urjeet
+ */
 
     (function ($) {
     "use strict"; // Start of use strict
