543a0d0c932923013abce8eecb96e0fcc30df1e5
Urjeet Deshmukh <60583154+urjeet@users.noreply.github.com>
2021-08-14 14:57:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { container, text_reveal, text_reveal_fade } from "../utils/Animation";
 import { Frame } from "../components";
 
 const About = () => {
-  const texts = [{ content: "One pixel at a time" }];
+  const texts = [{ content: "Urjeet Deshmukh" }];
 
   const textList = texts.map((text, index) => (
     <div className="overflow-hidden" key={index}>
@@ -27,19 +27,7 @@ const About = () => {
         <motion.div variants={container} className="md:w-1/2 lg:pl-36">
           {textList}
           <motion.p variants={text_reveal_fade}>
-            I build intuitive digital interfaces, offering a memorable user
-            experience. I am a professional Front-end developer and my area of
-            expertise is <a className="text-blue-400">ReactJs 🎗</a>,{" "}
-            <a className="text-pink-400">GraphQl</a>,{" "}
-            <a className="text-green-400">Typescript ♥️</a>,{" "}
-            <a className="text-pink-400">Scss/Css</a>,{" "}
-            <a className="text-blue-400">Redux</a>,{" "}
-            <a className="text-green-400">C </a>,{" "}
-            <a className="text-pink-400">Javascript</a>,{" "}
-            <a className="text-green-400">Algorithms and data structure</a>,{" "}
-            <a className="text-pink-400">RestFull Api</a> and{" "}
-            <a className="text-blue-400">NodeJs ✨</a>.
-            My whole life is shaped around my passion for web programming.
+            print("Hello, World!") My name is Urjeet Deshmukh and I am currently a sophomore studying Computer Science at the University of Pittsburgh. As a budding, yet ambitious software developer, I am most passionate about all aspects of data science, software engineering and machine learning. However, I would also enjoy exploring other areas of the field and am always ready to solve challenging problems. I plan to supplement my academics with a minor in Economics. Apart from my concentrations, my interests also lie in the subjects of psychology, cognition, and neuroscience
           </motion.p>
         </motion.div>
         <div className="md:static fixed md:block w-1/2">
